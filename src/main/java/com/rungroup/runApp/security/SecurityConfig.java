2f9fdb3238bccced88f5a61c429773bfe96f97f9
David Onyeka <105269888+davetech112@users.noreply.github.com>
2023-05-04 16:34:22 +0100

    started implementing spring security contd

--- src/main/java/com/rungroup/runApp/security/SecurityConfig.java
+++ src/main/java/com/rungroup/runApp/security/SecurityConfig.java
@@ -1,15 +1,30 @@
 package com.rungroup.runApp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailService customUserDetailService) {
+        this.customUserDetailService = customUserDetailService;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable()
@@ -28,5 +43,8 @@ public class SecurityConfig {
                                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
                 );
         return http.build();
+    }
+    public void configure(AuthenticationManagerBuilder builder) throws Exception{
+        builder.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
     }
 }
